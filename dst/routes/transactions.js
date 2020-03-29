2f39bce05df3eaaead07958767c8dce7744157dc
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-03-29 07:28:29 +0000

    ### Changed - UI調整

--- dst/routes/transactions.js
+++ dst/routes/transactions.js
@@ -32,24 +32,44 @@ transactionsRouter.get('/', (req, _, next) => __awaiter(void 0, void 0, void 0, 
     }
 }));
 /**
- * 入金取引開始
+ * 取引開始
  */
-transactionsRouter.all('/deposit/start', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+transactionsRouter.all('/start', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         let values = {};
-        let message;
+        let message = '';
         if (req.method === 'POST') {
             values = req.body;
             try {
-                const depositTransactionService = new pecorinoapi.service.transaction.Deposit({
-                    endpoint: process.env.API_ENDPOINT,
-                    auth: req.user.authClient
-                });
-                const startParams = createStartParams(req);
-                const transaction = yield depositTransactionService.start(startParams);
+                let transaction;
+                switch (req.body.transactionType) {
+                    case pecorinoapi.factory.transactionType.Deposit:
+                        const depositService = new pecorinoapi.service.transaction.Deposit({
+                            endpoint: process.env.API_ENDPOINT,
+                            auth: req.user.authClient
+                        });
+                        transaction = yield depositService.start(createStartParams(req));
+                        break;
+                    case pecorinoapi.factory.transactionType.Transfer:
+                        const transferService = new pecorinoapi.service.transaction.Transfer({
+                            endpoint: process.env.API_ENDPOINT,
+                            auth: req.user.authClient
+                        });
+                        transaction = yield transferService.start(createStartParams(req));
+                        break;
+                    case pecorinoapi.factory.transactionType.Withdraw:
+                        const withdrawService = new pecorinoapi.service.transaction.Withdraw({
+                            endpoint: process.env.API_ENDPOINT,
+                            auth: req.user.authClient
+                        });
+                        transaction = yield withdrawService.start(createStartParams(req));
+                        break;
+                    default:
+                        throw new Error(`Transaction type ${req.body.transactionType} not implemented`);
+                }
                 // セッションに取引追加
                 req.session[`transaction:${transaction.id}`] = transaction;
-                res.redirect(`/projects/${req.project.id}/transactions/deposit/${transaction.id}/confirm`);
+                res.redirect(`/projects/${req.project.id}/transactions/${transaction.id}/confirm`);
                 return;
             }
             catch (error) {
@@ -64,7 +84,7 @@ transactionsRouter.all('/deposit/start', (req, res, next) => __awaiter(void 0, v
             project: { id: { $eq: req.project.id } },
             inCodeSet: { identifier: { $eq: chevreapi.factory.categoryCode.CategorySetIdentifier.AccountType } }
         });
-        res.render('transactions/deposit/start', {
+        res.render('transactions/start', {
             values: values,
             message: message,
             accountTypes: searchAccountTypesResult.data
@@ -75,201 +95,9 @@ transactionsRouter.all('/deposit/start', (req, res, next) => __awaiter(void 0, v
     }
 }));
 /**
- * 入金取引確認
+ * 取引確認
  */
-transactionsRouter.all('/deposit/:transactionId/confirm', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        let message;
-        let toAccount;
-        const transaction = req.session[`transaction:${req.params.transactionId}`];
-        if (transaction === undefined) {
-            throw new pecorinoapi.factory.errors.NotFound('Transaction in session');
-        }
-        if (req.method === 'POST') {
-            // 確定
-            const depositTransactionService = new pecorinoapi.service.transaction.Deposit({
-                endpoint: process.env.API_ENDPOINT,
-                auth: req.user.authClient
-            });
-            yield depositTransactionService.confirm(transaction);
-            debug('取引確定です。');
-            message = '入金取引を実行しました。';
-            // セッション削除
-            delete req.session[`transaction:${req.params.transactionId}`];
-            req.flash('message', '入金取引を実行しました。');
-            res.redirect(`/projects/${req.project.id}/transactions/deposit/start`);
-            return;
-        }
-        else {
-            // 入金先口座情報を検索
-            const accountService = new pecorinoapi.service.Account({
-                endpoint: process.env.API_ENDPOINT,
-                auth: req.user.authClient
-            });
-            const searchAccountsResult = yield accountService.search({
-                accountType: transaction.object.toLocation.accountType,
-                accountNumbers: [transaction.object.toLocation.accountNumber],
-                statuses: [],
-                limit: 1
-            });
-            toAccount = searchAccountsResult.data.shift();
-            if (toAccount === undefined) {
-                throw new Error('To Location Not Found');
-            }
-        }
-        res.render('transactions/deposit/confirm', {
-            transaction: transaction,
-            message: message,
-            toAccount: toAccount
-        });
-    }
-    catch (error) {
-        next(error);
-    }
-}));
-/**
- * 出金取引開始
- */
-transactionsRouter.all('/withdraw/start', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        let values = {};
-        let message;
-        if (req.method === 'POST') {
-            values = req.body;
-            try {
-                const withdrawService = new pecorinoapi.service.transaction.Withdraw({
-                    endpoint: process.env.API_ENDPOINT,
-                    auth: req.user.authClient
-                });
-                const startParams = createStartParams(req);
-                const transaction = yield withdrawService.start(startParams);
-                // セッションに取引追加
-                req.session[`transaction:${transaction.id}`] = transaction;
-                res.redirect(`/projects/${req.project.id}/transactions/withdraw/${transaction.id}/confirm`);
-                return;
-            }
-            catch (error) {
-                message = error.message;
-            }
-        }
-        const categoryCodeService = new chevreapi.service.CategoryCode({
-            endpoint: process.env.CHEVRE_API_ENDPOINT,
-            auth: req.user.authClient
-        });
-        const searchAccountTypesResult = yield categoryCodeService.search({
-            project: { id: { $eq: req.project.id } },
-            inCodeSet: { identifier: { $eq: chevreapi.factory.categoryCode.CategorySetIdentifier.AccountType } }
-        });
-        res.render('transactions/withdraw/start', {
-            values: values,
-            message: message,
-            accountTypes: searchAccountTypesResult.data
-        });
-    }
-    catch (error) {
-        next(error);
-    }
-}));
-/**
- * 出金取引確認
- */
-transactionsRouter.all('/withdraw/:transactionId/confirm', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        let message;
-        let fromAccount;
-        const transaction = req.session[`transaction:${req.params.transactionId}`];
-        if (transaction === undefined) {
-            throw new pecorinoapi.factory.errors.NotFound('Transaction in session');
-        }
-        if (req.method === 'POST') {
-            // 確定
-            const withdrawService = new pecorinoapi.service.transaction.Withdraw({
-                endpoint: process.env.API_ENDPOINT,
-                auth: req.user.authClient
-            });
-            yield withdrawService.confirm(transaction);
-            debug('取引確定です。');
-            message = '出金取引を実行しました。';
-            // セッション削除
-            delete req.session[`transaction:${req.params.transactionId}`];
-            req.flash('message', '出金取引を実行しました。');
-            res.redirect(`/projects/${req.project.id}/transactions/withdraw/start`);
-            return;
-        }
-        else {
-            // 入金先口座情報を検索
-            const accountService = new pecorinoapi.service.Account({
-                endpoint: process.env.API_ENDPOINT,
-                auth: req.user.authClient
-            });
-            const searchAccountsResult = yield accountService.search({
-                accountType: transaction.object.fromLocation.accountType,
-                accountNumbers: [transaction.object.fromLocation.accountNumber],
-                statuses: [],
-                limit: 1
-            });
-            fromAccount = searchAccountsResult.data.shift();
-            if (fromAccount === undefined) {
-                throw new Error('From Location Not Found');
-            }
-        }
-        res.render('transactions/withdraw/confirm', {
-            transaction: transaction,
-            message: message,
-            fromAccount: fromAccount
-        });
-    }
-    catch (error) {
-        next(error);
-    }
-}));
-/**
- * 転送取引開始
- */
-transactionsRouter.all('/transfer/start', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        let values = {};
-        let message;
-        if (req.method === 'POST') {
-            values = req.body;
-            try {
-                const transferService = new pecorinoapi.service.transaction.Transfer({
-                    endpoint: process.env.API_ENDPOINT,
-                    auth: req.user.authClient
-                });
-                const startParams = createStartParams(req);
-                const transaction = yield transferService.start(startParams);
-                // セッションに取引追加
-                req.session[`transaction:${transaction.id}`] = transaction;
-                res.redirect(`/projects/${req.project.id}/transactions/transfer/${transaction.id}/confirm`);
-                return;
-            }
-            catch (error) {
-                message = error.message;
-            }
-        }
-        const categoryCodeService = new chevreapi.service.CategoryCode({
-            endpoint: process.env.CHEVRE_API_ENDPOINT,
-            auth: req.user.authClient
-        });
-        const searchAccountTypesResult = yield categoryCodeService.search({
-            project: { id: { $eq: req.project.id } },
-            inCodeSet: { identifier: { $eq: chevreapi.factory.categoryCode.CategorySetIdentifier.AccountType } }
-        });
-        res.render('transactions/transfer/start', {
-            values: values,
-            message: message,
-            accountTypes: searchAccountTypesResult.data
-        });
-    }
-    catch (error) {
-        next(error);
-    }
-}));
-/**
- * 転送取引確認
- */
-transactionsRouter.all('/transfer/:transactionId/confirm', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+transactionsRouter.all('/:transactionId/confirm', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         let message;
         let fromAccount;
@@ -280,17 +108,37 @@ transactionsRouter.all('/transfer/:transactionId/confirm', (req, res, next) => _
         }
         if (req.method === 'POST') {
             // 確定
-            const transferService = new pecorinoapi.service.transaction.Transfer({
-                endpoint: process.env.API_ENDPOINT,
-                auth: req.user.authClient
-            });
-            yield transferService.confirm(transaction);
+            switch (transaction.typeOf) {
+                case pecorinoapi.factory.transactionType.Deposit:
+                    const depositService = new pecorinoapi.service.transaction.Deposit({
+                        endpoint: process.env.API_ENDPOINT,
+                        auth: req.user.authClient
+                    });
+                    yield depositService.confirm(transaction);
+                    break;
+                case pecorinoapi.factory.transactionType.Transfer:
+                    const transferService = new pecorinoapi.service.transaction.Transfer({
+                        endpoint: process.env.API_ENDPOINT,
+                        auth: req.user.authClient
+                    });
+                    yield transferService.confirm(transaction);
+                    break;
+                case pecorinoapi.factory.transactionType.Withdraw:
+                    const withdrawService = new pecorinoapi.service.transaction.Withdraw({
+                        endpoint: process.env.API_ENDPOINT,
+                        auth: req.user.authClient
+                    });
+                    yield withdrawService.confirm(transaction);
+                    break;
+                default:
+                    throw new Error(`Transaction type ${req.body.transactionType} not implemented`);
+            }
             debug('取引確定です。');
-            message = '転送取引を実行しました。';
+            message = '取引を実行しました。';
             // セッション削除
             delete req.session[`transaction:${req.params.transactionId}`];
-            req.flash('message', '転送取引を実行しました。');
-            res.redirect(`/projects/${req.project.id}/transactions/transfer/start`);
+            req.flash('message', '取引を実行しました。');
+            res.redirect(`/projects/${req.project.id}/transactions/start`);
             return;
         }
         else {
@@ -299,25 +147,29 @@ transactionsRouter.all('/transfer/:transactionId/confirm', (req, res, next) => _
                 endpoint: process.env.API_ENDPOINT,
                 auth: req.user.authClient
             });
-            let searchAccountsResult = yield accountService.search({
-                accountType: transaction.object.fromLocation.accountType,
-                accountNumbers: [transaction.object.fromLocation.accountNumber],
-                statuses: [],
-                limit: 1
-            });
-            fromAccount = searchAccountsResult.data.shift();
-            searchAccountsResult = yield accountService.search({
-                accountType: transaction.object.toLocation.accountType,
-                accountNumbers: [transaction.object.toLocation.accountNumber],
-                statuses: [],
-                limit: 1
-            });
-            toAccount = searchAccountsResult.data.shift();
-            if (toAccount === undefined) {
-                throw new Error('To Location Not Found');
+            if (transaction.object.fromLocation !== undefined) {
+                const searchAccountsResult = yield accountService.search({
+                    accountType: transaction.object.fromLocation.accountType,
+                    accountNumbers: [transaction.object.fromLocation.accountNumber],
+                    statuses: [],
+                    limit: 1
+                });
+                fromAccount = searchAccountsResult.data.shift();
+            }
+            if (transaction.object.toLocation !== undefined) {
+                const searchAccountsResult = yield accountService.search({
+                    accountType: transaction.object.toLocation.accountType,
+                    accountNumbers: [transaction.object.toLocation.accountNumber],
+                    statuses: [],
+                    limit: 1
+                });
+                toAccount = searchAccountsResult.data.shift();
+                if (toAccount === undefined) {
+                    throw new Error('To Location Not Found');
+                }
             }
         }
-        res.render('transactions/transfer/confirm', {
+        res.render('transactions/confirm', {
             transaction: transaction,
             message: message,
             fromAccount: fromAccount,
